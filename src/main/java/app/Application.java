b116e464582b84cbd18913b1d2838000eb81600c
David Sargsyan <daviddaviddavka0715@mail.ru>
2022-10-04 12:51:18 +0300

    First Commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -9,12 +9,11 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+            new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+                applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
-
 }
